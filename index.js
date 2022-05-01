0bf0979cc3c264910de2eb96a8bbadf9ec6e9c1b
Jide Adesanya <Jide1012@yahoo.com>
2022-05-01 11:32:52 -0500

    installed inquire to Read-Me

---
+++ index.js
@@ -0,0 +1,102 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const util = require('util')
+const { type } = require('os')
+
+var inquirer = require('inquirer');
+const { title } = require('process');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        name: "Title",
+        message:"What is the title of your project",
+        type:"input",
+        validate: titleInput =>  {
+            if(titleInput){
+                return true;
+            }
+            else{
+                console.log('Please include a title');
+                return false;
+            }
+        }
+        
+    },{  
+
+        name: "Description",
+        message:"Give a short description of your project",
+        type:"input",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            }
+            else{
+                console.log('Include Description');
+                return false;
+
+            }
+        }
+        
+
+    },{
+        
+        name: "Installation",
+        message:"What instructions should the users follow to install your application",
+        type:"input",
+        validate: installationInput => {
+            if (installationInput){
+                return true
+
+            }
+            else {
+                console.log('Please')
+
+            }
+
+        }
+
+    },{
+        name: "Usage",
+        message:"How to use this application?",
+        type:"input",
+        validate: usageInput => {
+            if (usageInput) {
+                return true
+            }
+            else{ 
+                console.log ('Please include sample of code');
+            }
+        }
+    },{
+
+
+        name: "License",
+        message:"Which of the follwoing license you used for this project",
+        type:"list",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0'],
+
+    },{ 
+        
+        name: "Contributing",
+        message:"How can someone contribute to this project?",
+        type:"input",
+
+        validate: contributingInput => {
+            if (contributingInput) {
+                return true;
+            }
+
+            else{
+                console.log('Please include how they would contribute')
+                return false;
+            }
+        }
+
+
+
+    }
+
+
+])
+ 
